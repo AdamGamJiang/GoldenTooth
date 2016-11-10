9b6f7103d0c35f2a68f2b03bbe0ec93ded2b0c38
gan <446716975@qq.com>
2016-11-10 15:50:09 +0800

    滑动返回，activity基类

---
+++ app/src/main/java/com/gan/goldentooth/goldentooth/constants/CommentConstant.java
@@ -0,0 +1,8 @@
+package com.gan.goldentooth.goldentooth.constants;
+
+/**
+ * Created by issuser on 2016/11/10.
+ */
+public class CommentConstant {
+    public static final String SP_NAME = "config";
+}
